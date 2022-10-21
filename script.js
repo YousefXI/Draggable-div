b32a233583fdb5a9e2d3a57b675362b8a49f61a1
YousefXI <101951776+YousefXI@users.noreply.github.com>
2022-10-21 23:12:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-const wrapper = document.querySelector("wrapper"),
-   header = wrapper.querySelector("header");
+const wrapper = document.querySelector("wrapper");
+const header = wrapper.querySelector("header");
+
+function onDrag() {
+   let getStyle = window.getComputedStyle(wrapper);
+   let left = getStyle.left;
+   let top = getStyle.top;
+   console.log(left, top);
+}
 
 header.addEventListener("mousedown", () => {
    header.addEventListener("mousemove", onDrag);
