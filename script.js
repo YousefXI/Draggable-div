35e609aa56de407ba559ab074d20796b18e277f0
YousefXI <101951776+YousefXI@users.noreply.github.com>
2022-10-30 22:41:08 +0100

    not functioning

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const wrapper = document.querySelector(".wrapper"),
    header = wrapper.querySelector(".header");
 
+function onDrag() {
+   let getStyle = window.getComputedStyle(wrapper);
+   let left = getStyle.left;
+   let top = getStyle.top;
+   console.log(typeof left, typeof top);
+}
+
 header.addEventListener("mousedown", () => {
    header.addEventListener("mousemove", onDrag);
 });
